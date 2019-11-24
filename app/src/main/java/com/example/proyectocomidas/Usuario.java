41498276874b452c408c9e80340c89508fa8382d
raulrp3 <raul.ramirez@escuelaestech.es>
2019-11-24 12:49:04 +0100

    Realizado acceso a la aplicación

---
+++ app/src/main/java/com/example/proyectocomidas/Usuario.java
@@ -0,0 +1,68 @@
+package com.example.proyectocomidas;
+
+public class Usuario {
+    private String nombre;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String password;
+
+    public Usuario(String nombre, String email, String direccion, String telefono, String password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
