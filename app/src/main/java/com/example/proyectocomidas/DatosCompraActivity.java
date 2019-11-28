b4ff67fa1d90ca22aff896a9eb2beaba46cea1cb
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2019-11-28 16:33:42 +0100

    Preparacion para realizar pedido

--- app/src/main/java/com/example/proyectocomidas/DatosCompraActivity.java
+++ app/src/main/java/com/example/proyectocomidas/DatosCompraActivity.java
@@ -4,20 +4,32 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DatosCompraActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firebaseFirestore;
     EditText nombreText, emailText, direccionText, telefonoText;
+    Spinner spinner;
+    Button btnPedido;
 
 
     @Override
@@ -34,6 +46,14 @@ public class DatosCompraActivity extends AppCompatActivity {
         emailText = findViewById(R.id.emailText);
         direccionText = findViewById(R.id.direccionText);
         telefonoText = findViewById(R.id.telefonoText);
+        btnPedido = findViewById(R.id.btnPedido);
+        spinner = findViewById(R.id.horaSelect);
+
+        ArrayList<String> horas = getHours();
+
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, horas));
+
+        //spinner.getSelectedItem().toString();
 
         if(firebaseAuth.getCurrentUser() != null){
 
@@ -42,6 +62,13 @@ public class DatosCompraActivity extends AppCompatActivity {
         }else{
             Log.e("Usuario", "No hay usuario logueado");
         }
+
+        btnPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doOrder();
+            }
+        });
     }
 
     private void getUser(final String email){
@@ -89,5 +116,107 @@ public class DatosCompraActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private ArrayList<String> getHours(){
+        int horaAperturaMañana = 9;
+        int minutosAperturaMañana = 0;
+        int horaAperturaTarde = 19;
+        int minutosAperturaTarde = 0;
+
+        int horaCierreMañana = 14;
+        int minutosCierreMañana = 30;
+        int horaCierreTarde = 22;
+        int minutosCierraTarde = 0;
+
+        ArrayList<String> horasRecogida = new ArrayList<>();
+
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+        Calendar calendarioAhora = Calendar.getInstance();
+        calendarioAhora.add(Calendar.HOUR_OF_DAY, 1);
+        // String hour = format.format(calendar.getTime());
+
+        Calendar calendarioCierreMañana = Calendar.getInstance();
+        calendarioCierreMañana.set(Calendar.HOUR_OF_DAY, horaCierreMañana);
+        calendarioCierreMañana.set(Calendar.MINUTE, minutosCierreMañana);
+
+        Calendar calendarioCierreTarde = Calendar.getInstance();
+        calendarioCierreTarde.set(Calendar.HOUR_OF_DAY, horaCierreTarde);
+        calendarioCierreTarde.set(Calendar.MINUTE, minutosCierraTarde);
+
+        Calendar calendarioAperturaTarde = Calendar.getInstance();
+        calendarioAperturaTarde.set(Calendar.HOUR_OF_DAY, horaAperturaTarde);
+        calendarioAperturaTarde.set(Calendar.MINUTE, minutosAperturaTarde);
+
+
+        Calendar calendarioAperturaMañana = Calendar.getInstance();
+        calendarioAperturaMañana.set(Calendar.HOUR_OF_DAY, horaAperturaMañana);
+        calendarioAperturaMañana.set(Calendar.MINUTE, minutosAperturaMañana);
+
+
+        if(calendarioAhora.before(calendarioAperturaMañana) || calendarioAhora.after(calendarioCierreTarde)){
+            calendarioAhora = calendarioAperturaMañana;
+
+            while(calendarioAhora.before(calendarioCierreMañana)){
+                horasRecogida.add(format.format(calendarioAhora.getTime()));
+                calendarioAhora.add(Calendar.MINUTE, 30);
+            }
+
+        } else if(calendarioAhora.before(calendarioCierreMañana)){
+            calendarioAhora.add(Calendar.MINUTE, 30);
+
+            while(calendarioAhora.before(calendarioCierreMañana)){
+
+                if(calendarioAhora.after(calendarioCierreMañana))
+                    break;
+
+                horasRecogida.add(format.format(calendarioAhora.getTime()));
+                calendarioAhora.add(Calendar.MINUTE, 30);
+            }
+
+        } else if(calendarioAhora.before(calendarioAperturaTarde)){
+            calendarioAhora = calendarioAperturaTarde;
+
+            while(calendarioAhora.before(calendarioCierreTarde)){
+                horasRecogida.add(format.format(calendarioAhora.getTime()));
+                calendarioAhora.add(Calendar.MINUTE, 30);
+            }
+
+        } else {
+            calendarioAhora.add(Calendar.MINUTE, 30);
+
+            while(calendarioAhora.before(calendarioCierreTarde)){
+
+                if(calendarioAhora.after(calendarioCierreTarde))
+                    break;
+
+                horasRecogida.add(format.format(calendarioAhora.getTime()));
+                calendarioAhora.add(Calendar.MINUTE, 30);
+            }
+        }
+
+        return horasRecogida;
+    }
+
+    private void doOrder(){
+        if(nombreText.getText().toString().isEmpty() || emailText.getText().toString().isEmpty() || direccionText.getText().toString().isEmpty() || telefonoText.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "No puede haber campos vacíos", Toast.LENGTH_SHORT).show();
+        } else {
+            String hora = "";
+
+            try{
+
+               hora =  spinner.getSelectedItem().toString();
+
+            }catch (Exception e){
+                Log.e("Error", e.getMessage());
+            }
+
+            if(hora.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Debes seleccionar una hora de recogida", Toast.LENGTH_SHORT).show();
+            } else {
+               // Timestamp fechaPedido = new Timestamp(new Date());
+            }
+        }
     }
 }
